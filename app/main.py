da1f174ab693097f73ce326968c083ecef15d0c0
Abhay S. Sandhu <abhay.sandhu07@gmail.com>
2024-06-13 17:42:59 +0530

    4th Stage

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 # Uncomment this to pass the first stage
 import socket
-
+import re
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -20,14 +20,26 @@ def main():
     request = data.decode('utf-8').splitlines()[0]
     print('Request line:', request)
 
+    pattern = re.compile(r"GET /echo/(\S+) HTTP/1\.1")
+    match = pattern.search(request)
+    
     if request == 'GET / HTTP/1.1':
         response = "HTTP/1.1 200 OK\r\n\r\n"
+    
+    elif match:
+        response = (f"HTTP/1.1 200 OK\r\n
+                    Content-Type: text/plain\r\n
+                    Content-Length: {len(match.group(1))}\r\n
+                    \r\n
+                    {match.group(1)}
+                    ")
+
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
     
     conn.sendall(response.encode('utf-8'))
     print('response sent')
-    
+
     conn.close()
     print(f"Connection with {addr} closed")
 
