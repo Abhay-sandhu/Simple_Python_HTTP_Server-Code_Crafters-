308d0d00b094d2b5fc7817bda6855ecca22a1d53
Abhay S. Sandhu <abhay.sandhu07@gmail.com>
2024-06-13 17:24:38 +0530

    3rd Stage(1)

--- app/main.py
+++ app/main.py
@@ -9,19 +9,27 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+    print("Server is listening on port 4221...")
+
     conn, addr = server_socket.accept() # wait for client
+    print(f"Accepted connection from {addr}")
+
     data = conn.recv(1024)
-    print('recieved_data:', data.decode('utf-8'))
+    print('Received data:', data.decode('utf-8'))
+
     request = data.decode('utf-8').splitlines()[0]
+    print('Request line:', request)
+
     if request == 'GET / HTTP/1.1':
-        response = (
-            "HTTP/1.1 200 OK\r\n\r\n"
-        )
+        response = "HTTP/1.1 200 OK\r\n\r\n"
     else:
-        response = (
-            "HTTP/1.1 404 Not Found\r\n\r\n"
-        )
+        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+    
     conn.sendall(response.encode('utf-8'))
+    print('response sent')
+    
     conn.close()
+    print(f"Connection with {addr} closed")
+
 if __name__ == "__main__":
     main()
