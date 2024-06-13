f35870cd349b17f2ba5a6a230d870d94971dec7f
Abhay S. Sandhu <abhay.sandhu07@gmail.com>
2024-06-13 17:20:06 +0530

    3rd Stage

--- app/main.py
+++ app/main.py
@@ -11,9 +11,15 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     conn, addr = server_socket.accept() # wait for client
     data = conn.recv(1024)
-    print(data.decode('utf-8'))
-    response = (
+    print('recieved_data:', data.decode('utf-8'))
+    request = data.decode('utf-8').splitlines()[0]
+    if request == 'GET / HTTP/1.1':
+        response = (
             "HTTP/1.1 200 OK\r\n\r\n"
+        )
+    else:
+        response = (
+            "HTTP/1.1 404 Not Found\r\n\r\n"
         )
     conn.sendall(response.encode('utf-8'))
     conn.close()
