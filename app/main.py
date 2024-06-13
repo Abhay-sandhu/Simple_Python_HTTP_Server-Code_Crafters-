dbb7dfd7bea631b8c3753e266a0d6648e91002f4
Abhay S. Sandhu <abhay.sandhu07@gmail.com>
2024-06-13 18:05:14 +0530

    5th Stage

--- app/main.py
+++ app/main.py
@@ -20,14 +20,32 @@ def main():
     request = data.decode('utf-8').splitlines()[0]
     print('Request line:', request)
 
-    pattern = re.compile(r"GET /echo/(\S+) HTTP/1\.1")
-    match = pattern.search(request)
+    echo_pattern = re.compile(r"GET /echo/(\S+) HTTP/1\.1")
+    match_echo = echo_pattern.search(request)
+
+    u_agent_pattern = re.compile(r"GET /user-agent HTTP/1\.1")
+    if u_agent_pattern.search(request):
+        user_agent_pattern = re.compile(r"User-Agent: (\S+)")
+        match_u_agent = user_agent_pattern.search(data.decode('utf-8'))
 
     if request == 'GET / HTTP/1.1':
         response = "HTTP/1.1 200 OK\r\n\r\n"
    
-    elif match:
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(match.group(1))}\r\n\r\n{match.group(1)}"
+    elif match_echo:
+        response = (f"HTTP/1.1 200 OK\r\n"
+                    f"Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(match_echo.group(1))}\r\n"
+                    f"\r\n"
+                    f"{match_echo.group(1)}"
+                    )
+    
+    elif match_u_agent:
+        response = (f"HTTP/1.1 200 OK\r\n"
+                    f"Content-Type: text/plain\r\n"
+                    f"Content-Length: {len(match_u_agent.group(1))}"
+                    f"\r\n"
+                    f"{match_u_agent.group(1)}"
+                    )
 
     else:
         response = "HTTP/1.1 404 Not Found\r\n\r\n"
