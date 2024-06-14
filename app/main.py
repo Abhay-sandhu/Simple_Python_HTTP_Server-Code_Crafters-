2ca0ab876020d5efe8ea9e9e14a005355aa3444d
Abhay S. Sandhu <abhay.sandhu07@gmail.com>
2024-06-14 12:06:35 +0530

    refactoring

--- app/main.py
+++ app/main.py
@@ -8,6 +8,14 @@ import gzip
 import binascii
 
 def main():
+    """
+    Starts a simple HTTP server that serves files from the specified directory.
+    
+    The server listens on localhost port 4221 and creates a new thread to handle each incoming client connection. The `handle_client` function is responsible for processing the client request and sending the appropriate response.
+    
+    The server can be stopped by pressing Ctrl+C.
+    """
+        
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
     
@@ -23,132 +31,175 @@ def main():
     print("Server is listening on port 4221...")
 
     # Loop for receiving connections
-    while True:
+    try:
+        while True:
 
-        # wait for client
-        conn, addr = server_socket.accept() 
-        
-        # create a new thread to handle the client
-        client_thread = threading.Thread(target=handle_client, args=(conn, addr, directory))
-        
-        # start the thread
-        client_thread.start()
+            # wait for client
+            conn, addr = server_socket.accept() 
+            
+            # create a new thread to handle the client
+            client_thread = threading.Thread(target=handle_client, args=(conn, addr, directory))
+            
+            # start the thread
+            client_thread.start()
 
+    except KeyboardInterrupt:
+        print("\nServer is shutting down...")
+    finally:
+        server_socket.close()
+
+
+def parse_request(data):
+    """
+    Parses an HTTP request from the given data.
     
+    Args:
+        data (bytes): The raw HTTP request data.
+    
+    Returns:
+        dict: A dictionary containing the parsed request information, including:
+            - method (str): The HTTP method (e.g. 'GET', 'POST').
+            - path (str): The requested path.
+            - version (str): The HTTP version (e.g. 'HTTP/1.1').
+            - req_content_header (dict): A dictionary of the request headers, with keys and values lowercased.
+            - req_content_body (str): The request body as a string.
+    """
+    try:
+        headers, body = data.split(b'\r\n\r\n', 1)
+    except ValueError:
+        headers, body = data, b""
+
+    header_lines = headers.decode('utf-8').splitlines()
+    request_line = header_lines[0]
+    method, path, version = request_line.split(" ")
+    content_header = header_lines[1:]
+
+    content_header_dict = {header.split(": ")[0].lower(): header.split(": ")[1] for header in content_header if ": " in header}
+
+    return {
+       'method': method,
+       'path': path,
+       'version': version,
+       'req_content_header': content_header_dict,
+       'req_content_body': body.decode('utf-8')
+    }
+
+
+def create_response(version, code, code_message, content_header={}, content_body=None):
+    """
+    Creates an HTTP response string with the given version, status code, status message, optional headers, and optional content body.
+    
+    Args:
+        version (str): The HTTP version (e.g. 'HTTP/1.1').
+        code (int): The HTTP status code (e.g. 200, 404).
+        code_message (str): The HTTP status message (e.g. 'OK', 'File Not Found').
+        content_header (dict, optional): A dictionary of additional HTTP headers to include in the response.
+        content_body (str, optional): The content body to include in the response.
+    
+    Returns:
+        str: The complete HTTP response string.
+    """ 
+    headers = f"{version} {code} {code_message}\r\n"
+    
+    if content_header:
+        for key, value in content_header.items():
+            headers += f"{key}: {value}\r\n"
+    
+    if content_body is not None:
+        headers += f"Content-Length: {len(content_body.encode('utf-8'))}\r\n"
+
+    headers += "\r\n"
+    
+    if content_body is not None:
+        response = headers + content_body
+    else:
+        response = headers
+    
+    return response
+
 
 
 def handle_client(conn, addr, directory):
+    """
+    Handles an incoming client connection, parsing the request and generating an appropriate response.
    
+    Args:
+       conn (socket.socket): The socket connection to the client.
+       addr (tuple): The address of the client.
+       directory (str): The directory to serve files from.
+   
+    Returns:
+        None
+   """ 
     print(f"Accepted connection from {addr}")
 
     # receive data
     data = conn.recv(1024)
     print('Received data:', data.decode('utf-8'))
 
-    # request line
-    request = data.decode('utf-8').splitlines()[0]
-    print('Request line:', request)
-
-    # echo pattern and match
-    echo_pattern = re.compile(r"GET /echo/(\S+) HTTP/1\.1")
-    match_echo = echo_pattern.search(request)
-
-    # user agent pattern and match
-    u_agent_pattern = re.compile(r"GET /user-agent HTTP/1\.1")
-    match_user_agent = u_agent_pattern.search(request)
-
-    # files pattern and match
-    files_pattern = re.compile(r"GET /files/(\S+) HTTP/1\.1")
-    match_file = files_pattern.search(request)
-
-    # POST files pattern and match
-    post_files_pattern = re.compile(r"POST /files/(\S+) HTTP/1\.1")
-    match_post_file = post_files_pattern.search(request)
-
-
-    # Server_response
-    if request == 'GET / HTTP/1.1': # root response
-
-        response = "HTTP/1.1 200 OK\r\n\r\n"
+    # request parsed into dictionary
+    request_dict = parse_request(data)
     
+    method = request_dict['method']
+    path = request_dict['path']
+    version = request_dict['version']
+    req_content_header = request_dict['req_content_header']
+    req_content_body = request_dict['req_content_body']
 
-    elif match_echo: # echo response
+    response = ""
 
-        encoding_pattern = re.compile(r"[Aa]ccept-[Ee]ncoding: ([\S\s]+)\r\n")
-        match_encoding = encoding_pattern.search(data.decode('utf-8'))
-        
-        if match_encoding is not None:
-            match_gzip = re.search(r"([Gg]zip)", match_encoding.group(1))
-        
-        if match_encoding is not None and match_gzip:
-
-            gzipped_data = gzip.compress(bytes(match_echo.group(1), 'utf-8'))
-            hex_encoded_content = binascii.hexlify(gzipped_data).decode('utf-8')
-            response = (f"HTTP/1.1 200 OK\r\n"
-                        f"Content-Encoding: {match_gzip.group(1)}\r\n"
-                        f"Content-Type: text/plain\r\n"
-                        f"Content-Length: {len(hex_encoded_content)}\r\n"
-                        f"\r\n"
-                        f"{hex_encoded_content}"
-                        )
-        else:
-            response = (f"HTTP/1.1 200 OK\r\n"
-                    f"Content-Type: text/plain\r\n"
-                    f"Content-Length: {len(match_echo.group(1))}\r\n"
-                    f"\r\n"
-                    f"{match_echo.group(1)}"
-                    )
-
-
-    elif match_user_agent: # user agent response
-
-        user_agent_pattern = re.compile(r"[Uu]ser-[Aa]gent: (\S+)")
-        match_u_agent = user_agent_pattern.search(data.decode('utf-8'))
-        
-        response = (f"HTTP/1.1 200 OK\r\n"
-                    f"Content-Type: text/plain\r\n"
-                    f"Content-Length: {len(match_u_agent.group(1))}\r\n"
-                    f"\r\n"
-                    f"{match_u_agent.group(1)}"
-                    )
-
-
-    elif match_file: # files response
-
-        filename = match_file.group(1)
+    # GET '/' response
+    if method == 'GET' and path == '/' :
+        response = create_response(version, 200, 'OK')
+    
+    # GET '/echo/' response
+    elif method == 'GET' and path.startswith('/echo/'):
+        response = create_response(version, 200, 'OK', 
+                                   content_header={'Content-Type': 'text/plain'},
+                                   content_body=path[6:]
+                                   )
+    
+    # GET '/user-agent' response
+    elif method == 'GET' and path.startswith('/user-agent'):
+        user_agent = req_content_header.get('user-agent', 'No User-Agent header found')
+        response = create_response(version, 200, 'OK', 
+                                   content_header={'Content-Type': 'text/plain'}, 
+                                   content_body=user_agent
+                                   )
+   
+    # GET '/files/' response
+    elif method == 'GET' and path.startswith('/files/'): 
+        filename = path[7:] # >> /files/<file_name>
         file_path = os.path.join(directory,filename)
-        
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            with open(file_path, 'rb') as file:
                 file_content = file.read()
-            
-            response = (
-                f"HTTP/1.1 200 OK\r\n"
-                f"Content-Type: application/octet-stream\r\n"
-                f"Content-Length: {len(file_content)}\r\n"
-                f"\r\n"
-                f"{file_content}"
-            )
+            response = create_response(version, 200, 'OK',
+                                       content_header={'Content-Type': 'application/octet-stream'},
+                                       content_body=file_content.decode('latin-1')
+                                       )
         else:
-            response = "HTTP/1.1 404 Not Found\r\n\r\n"
+            # requested File does not Exist
+            response = create_response(version, 404, 'File Not Found')
 
-
-    elif match_post_file: # post files response
-
-        filename = match_post_file.group(1)
+    # POST '/files/' response
+    elif method == 'POST' and path.startswith('/files/'):
+        filename = path[7:] # >> /files/<file_name>
         file_path = os.path.join(directory,filename)
-        file_content = data.decode('utf-8').splitlines()[-1]
-        
+        file_content = req_content_body
         with open(file_path, 'w') as file:
             file.write(file_content)
-        
-        response = "HTTP/1.1 201 Created\r\n\r\n"
-
-        
-    else: # 'not found' response
-
-        response = "HTTP/1.1 404 Not Found\r\n\r\n"
+        response = create_response(version, 201, 'Created')
     
+    # invalid Method response
+    elif method not in ['GET', 'POST']:
+        response = create_response(version, 405, 'Method Not Allowed')
+    
+    # invalid Path response
+    else:
+        response = create_response(version, 404, 'Not Found')
+
+
     # send response
     conn.sendall(response.encode('utf-8'))
     print('response sent')
